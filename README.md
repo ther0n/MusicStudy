bad6090327b3c94df76aea18cb63e4a8d994bb3b
Theron Tjapkes <theron@zmagnet.com>
2018-12-17 19:41:05 -0500

    A few fixes and switched format to ogg which works better with pygame

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
 # MusicStudy
-Quick program to help study for MUSC-103
+Quick program to help study for MUSC-103. Plays a random 30 second clip from a random song.
 
-Song file names must follow the format `[Title]-[People/Form]-[Composer/Country]-[Category].flac`
+Song file names must follow the format `[Title]-[People/Form]-[Composer/Country]-[Category].ogg`
 
-Place songs (in flac format) into a folder named `songs` and run the program
+Place songs (in ogg vorbis format) into a folder named `songs` and run the program
 
 EXE file built with pyinstaller: `pyinstaller -F main.py`
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import sys
 import sox
 import os, random
+import time
 from pygame import mixer
 
 def main():
@@ -9,28 +10,32 @@ def main():
     
     while(True):
         input("\nPress enter to play the next clip")
-        os.remove("song.wav")
+        try:
+            os.remove("song.wav")
+        except OSError:
+            pass
         next_song = pick_song()
         duration = sox.file_info.duration(next_song)
         max_start = round(duration, 0)-30
         start = random.randint(0, max_start)
         clip = sox.Transformer()
         clip.trim(start,start+30)
+        clip.convert(44100)
         clip.build(next_song, "song.wav")
         sound = mixer.Sound("song.wav")
         sound.play()
         input("Press enter to reveal the song info")
         mixer.stop()
         # Song file name must be formatted "[Title]-[People/Form]-[Composer/Country]-[Category].flac"
-        song_info = next_song.split("/")[1].split(".")[0].split("-")
+        song_info = os.path.splitext(next_song.split("/")[1])[0].split("-")
         print("\n\tTitle: " + song_info[0] + 
               "\n\tPeople/Form: " + song_info[1] +
               "\n\tComposer/Country: " + song_info[2] +
               "\n\tCategory: " + song_info[3])
-        
+
 def pick_song():
     song = random.choice(os.listdir(os.getcwd() + "/songs"))
-    while(not song.endswith(".flac")):
+    while(not song.endswith(".ogg")):
         song = random.choice(os.listdir(os.getcwd() + "/songs"))
     return "songs/" + song
 
